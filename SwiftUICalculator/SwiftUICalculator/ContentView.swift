cf362522ceb92cb4f9dc04a7897e0fd3780c1222
ratneshShukla <>
2020-07-26 23:12:31 +0700

    Calculator UI building
    #100DaysOfCode

---
+++ SwiftUICalculator/SwiftUICalculator/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by Ratnesh Shukla on 26/07/20.
+//  Copyright © 2020 Ratnesh Shukla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let buttons = [
+    ["7", "8", "9", "/"],
+    ["4", "5", "6", "X"],
+    ["1", "2", "3", "+"],
+    ["0", ".", ".", "="]
+    ]
+    
+    var body: some View {
+        
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 12) {
+                HStack {
+                    Spacer()
+                    Text("42").foregroundColor(.white).font(.system(size: 64))
+                }.padding()
+
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id:\.self) { button in
+                            Text(button).font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(self.buttonWidth())
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5*12) / 4
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
