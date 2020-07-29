0d940c45a107e3553d3ac20ca12000f6516c21e8
ratneshShukla <ratneshkumarshukla07@gmail.com>
2020-07-29 22:50:36 +0700

    RestaurantApp: first commit

---
+++ Restaurant/Restaurant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaurant
+//
+//  Created by Ratnesh Shukla on 29/07/20.
+//  Copyright © 2020 Ratnesh Shukla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
