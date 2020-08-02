ee5accf410d8e0529226c8474412cbdb7c083779
ratneshShukla <ratneshkumarshukla07@gmail.com>
2020-08-02 23:51:24 +0700

    Environment and Observable object added.

--- Restaurant/Restaurant/ItemDetails.swift
+++ Restaurant/Restaurant/ItemDetails.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct ItemDetails: View {
+    @EnvironmentObject var order: Order
     var item: MenuItem
     var body: some View {
         VStack {
@@ -21,15 +22,21 @@ struct ItemDetails: View {
                     .foregroundColor(.white)
                     .offset(x:-5, y:-5)
             }
-            Text(item.description)
+            Text(item.description).padding()
+            Button("Order This") {
+                self.order.add(item: self.item)
+            }.font(.headline)
+            Spacer()
         }.navigationBarTitle(Text(item.name), displayMode: .inline)
     }
 }
 
 struct ItemDetails_Previews: PreviewProvider {
+    
+    static let order = Order()
     static var previews: some View {
         NavigationView {
-            ItemDetails(item: MenuItem.example)
+            ItemDetails(item: MenuItem.example).environmentObject(order)
         }
     }
 }
