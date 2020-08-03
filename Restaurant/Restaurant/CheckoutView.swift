22ac08349c428e3f469a4ffb35ed5861c3d94247
ratneshShukla <ratneshkumarshukla07@gmail.com>
2020-08-03 23:52:24 +0700

    CheckoutView added.

---
+++ Restaurant/Restaurant/CheckoutView.swift
@@ -0,0 +1,24 @@
+//
+//  CheckoutView.swift
+//  Restaurant
+//
+//  Created by Ratnesh Shukla on 03/08/20.
+//  Copyright © 2020 Ratnesh Shukla. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        CheckoutView().environmentObject(order)
+    }
+}
