0f163b647efa9444bff4a26821410c4885ac8727
ratneshShukla <ratneshkumarshukla07@gmail.com>
2020-08-05 23:52:52 +0700

    tipAmout, PickerView added.

--- Restaurant/Restaurant/CheckoutView.swift
+++ Restaurant/Restaurant/CheckoutView.swift
@@ -11,10 +11,18 @@ import SwiftUI
 struct CheckoutView: View {
     @EnvironmentObject var order: Order
     static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    static let tipAmounts = [10, 15, 20, 25, 0]
+
     @State private var paymentType = 0
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
-    
+    @State private var tipAmount = 1
+
+    var totalPrice: Double {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
     var body: some View {
         Form {
             Section {
@@ -28,6 +36,18 @@ struct CheckoutView: View {
                 }
                 if addLoyaltyDetails {
                     TextField("iDine Id", text: $loyaltyNumber)
+                }
+            }
+            Section(header: Text("Add Tip")) {
+                Picker("Percent", selection: $tipAmount) {
+                    ForEach(0 ..< Self.tipAmounts.count)  {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header: Text("Total Price: $\(totalPrice, specifier: "% .2f")")) {
+                Button("Confirm Order") {
+                    
                 }
             }
         }
