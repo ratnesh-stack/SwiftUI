0e50f0f16eb38184c88d8be779f32e8facf55df5
ratneshShukla <ratneshkumarshukla07@gmail.com>
2020-08-13 23:53:06 +0700

    WeatherView Attached.

--- WeatherApp/WeatherApp/Service/WeatherService.swift
+++ WeatherApp/WeatherApp/Service/WeatherService.swift
@@ -10,12 +10,15 @@ import Foundation
 
 class WeatherService {
     func getWeather(city: String, completion:@escaping(WeatherModel?) -> ()) {
-        let apiUrl = "https://samples.openweathermap.org/data/2.5/weather?q=\(city)&appid=439d4b804bc8187953eb36d2a8c26a02"
-        guard let url = URL(string: apiUrl) else { return }
+        let urlString = "https://samples.openweathermap.org/data/2.5/weather?q=\(city)&appid=439d4b804bc8187953eb36d2a8c26a02"
+        guard let url = URL(string: urlString) else { return }
+        
         URLSession.shared.dataTask(with: url) { data, resp, error in
             guard let data = data, error == nil else { return }
             guard let response =  try? JSONDecoder().decode(WeatherModel.self, from: data) else { completion(nil); return }
-            completion(response)
+            DispatchQueue.main.async {
+                completion(response)
+            }
         }.resume()
     }
 }
